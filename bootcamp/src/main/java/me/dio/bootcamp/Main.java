1ca6d3d30555bfe34359ba5336b95225696c7011
Alysson Rodrigues <rodrigues.alysson@gmail.com>
2022-02-17 17:06:29 -0300

    ajustes

--- bootcamp/src/main/java/me/dio/bootcamp/Main.java
+++ bootcamp/src/main/java/me/dio/bootcamp/Main.java
@@ -10,6 +10,8 @@ import me.dio.bootcamp.dominio.Mentoria;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
 		Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
